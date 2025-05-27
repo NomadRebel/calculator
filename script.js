5ae196d029d57daeea6065a0d7ef8b1b268f559e
NomadRebel <nikhilverma4209@gmail.com>
2025-05-27 12:07:46 +0530

    it kinda works added the variables to store the clicked button and passed it to the operation function

--- script.js
+++ script.js
@@ -19,7 +19,13 @@ function divison(a,b){
   }
 }
 
-function operation(a,b,c){
+function operation(x,y,z){
+  let a = Number(x);
+  let b = (y);
+  let c = Number(z);
+  console.log(a);
+  console.log(b);
+  console.log(c);
   if(b=== '+'){
     return (sum(a,c));
   }
@@ -33,13 +39,40 @@ function operation(a,b,c){
     return(divison(a,c));
   }
 }
+let input1 = 0;
+let clickCount = 0;
+let input2 = 0;
+let input3 = 0;
+let input4 =0;
 
 document.querySelectorAll('button').forEach(button => {
   button.addEventListener("click", () => {
+    clickCount++;
+    console.log(clickCount);
     document.getElementById("item").value=button.value;
-    const input1 = document.getElementById("item").value;
-    console.log(input1);
-    const input2 = document.getElementById("item").value;
-    console.log(input2);
+    if(clickCount===1){
+      input1=document.getElementById("item").value;
+      console.log(input1);
+    };
+    if(clickCount===2){
+      input2=document.getElementById("item").value;
+      console.log (input2);
+    }
+    if(clickCount===3){
+      input3=document.getElementById("item").value;
+      console.log (input3);
+    }
+    if(clickCount===4){
+      input4=document.getElementById("item").value;
+      if(input4==="="){
+        clickCount===0;
+        let vaue = document.getElementById("item").value
+        vaue = (operation(input1,input2,input3));
+        document.getElementById("item").value = vaue;
+      }
+    }
   });
 });
+
+console.log(input1);
+console.log(input2);
