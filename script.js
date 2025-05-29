ef445c676d856650a84aa0ef2ac8115f71f5cc3b
NomadRebel <nikhilverma4209@gmail.com>
2025-05-29 07:41:31 +0530

    finished

--- script.js
+++ script.js
@@ -39,4 +39,61 @@ function operation(x,y,z){
     return(divison(a,c));
   }
 }
+let a = '';
+let b = '';
+let c = '';
+let result = '';
+let display = document.getElementById("item");
 
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let val = button.value;
+
+    if (!isNaN(val) || val === '.') {
+  if (b === '') {
+    //  one decimal in operand a
+    if (val === '.' && a.includes('.')) return;
+    a += val;
+    display.value = a;
+  } else {
+    //  one decimal in operand c
+    if (val === '.' && c.includes('.')) return;
+    c += val;
+    display.value = c;
+  }
+} else if (['+', '-', '*', '/'].includes(val)) {
+      // if it's an operator
+      if (a !== '') {
+        b = val;
+      }
+    } else if (val === '=') {
+      if (a !== '' && b !== '' && c !== '') {
+        result = operation(a, b, c);
+        display.value = result;
+        
+        a = result.toString();
+        b = '';
+        c = '';
+      }
+    } else if (val === '') {
+      // clear button
+      a = '';
+      b = '';
+      c = '';
+      result = '';
+      display.value = '';
+    } else if (val === 'back') {
+      // backspace
+      if (c !== '') {
+        c = c.slice(0, -1);
+        display.value = c;
+      } else if (b !== '') {
+        b = '';
+      } else if (a !== '') {
+        a = a.slice(0, -1);
+        display.value = a;
+      }
+     }
+  });
+});
